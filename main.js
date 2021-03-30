89ddd90329abe6474a9c4afd03033859fddf06c9
alexpintodiaz <alexanderp143@hotmail.com>
2021-03-29 22:10:07 -0500

    26_03_2021_variables_constantes_tipos_de_datos

---
+++ main.js
@@ -0,0 +1,20 @@
+function Saludar(p1){
+    console.log(`Hola ${p1}`);
+    return null;
+}
+
+Saludar("Alexander");
+Saludar("Pinto");
+Saludar("Diaz");
+
+
+// Codigo en secuencia.
+let saludar;
+saludar = "Alexander Pinto";
+console.warn(`Hola ${saludar}`);
+saludar = "Carlos Andres";
+console.warn(`Hola ${saludar}`);
+saludar = "Astrid Diaz";
+console.warn(`Hola ${saludar}`);
+saludar = "Paula Daniela";
+console.warn(`Hola ${saludar}`);
